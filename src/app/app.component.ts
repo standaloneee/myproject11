da5b726b5dc7a6151b247eec4f21c32af9584ff8
standalone1337 <tzherzdev@gmail.com>
2021-05-27 20:44:30 +0300

    please work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Note } from './interface';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project11';
+  article = '';
+  maintext = '';
+  notes: Note[]=[];
+  idnumber = 1;
+
+  pushtext() {
+  
+    let note: Note = { id: this.idnumber++, name: this.article, maintext: this.maintext, date: new Date() }
+    this.notes.push(note);
+    console.log(this.notes);
+  }
+  delete(index:number) {
+    this.notes.splice(index,1);
+  }
 }
